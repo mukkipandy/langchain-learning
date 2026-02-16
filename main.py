a9a05471e41f938f0809e513265516a4e4c6c348
Mukesh Pandey <mukesh.mtr@gmail.com>
2026-02-16 15:49:00 +0530

    making changes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from typing import List
+
 from dotenv import load_dotenv
-from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 load_dotenv()
+
 
 class Source(BaseModel):
     """Schema for a source used by the agent"""
@@ -41,7 +43,11 @@ def search(query: str) -> str:
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 tools = [tavily_search_tool]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    response_format=AgentResponse
+)
 
 
 def main():
